c622e7d2bb1aeccb6b9c3a857c9d3ba847d1caa6
Tony <tonyguarino1489@gmail.com>
2023-05-12 22:31:40 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(() => {
+    const 
+        carouselWrapper = $('.carousel-inner'),
+        carouselWrapperInnerImage = $('.carousel-inner').find('img'),
+        carouselItem = $('.carousel-item'),
+        carouselItemInnerImage = $('carousel-item').find('img')
+        
+})
